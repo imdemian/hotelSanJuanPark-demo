5749ef592fc97fb07020a6029574731e95caa15b
imdemian <cdemian79@gmail.com>
2025-04-08 18:58:26 -0600

    modulo de reservaciones listo y arreglo del sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ProveedoresComponent } from './pages/proveedor/proveedor.component';
 import { ClientesComponent } from './pages/clientes/clientes/clientes.component';
 import { EmpleadosComponent } from './pages/empleados/empleados.component';
+import { ReservacionesComponent } from './pages/reservaciones/reservaciones.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'proveedores', pathMatch: 'full' },
   { path: 'proveedores', component: ProveedoresComponent },
   { path: 'clientes', component: ClientesComponent },
-  { path: 'empleados', component: EmpleadosComponent }
+  { path: 'empleados', component: EmpleadosComponent },
+  { path: 'reservaciones', component: ReservacionesComponent },
 ];
