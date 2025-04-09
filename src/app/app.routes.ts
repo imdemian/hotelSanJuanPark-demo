3995d5296a3aa9f1c7e23cfeaab9e975caedafe3
alisonHernandez <alisonhm.ti22@utsjr.edu.mx>
2025-04-08 23:57:47 -0600

    servicios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,18 @@ import { ProveedoresComponent } from './pages/proveedor/proveedor.component';
 import { ClientesComponent } from './pages/clientes/clientes/clientes.component';
 import { EmpleadosComponent } from './pages/empleados/empleados.component';
 import { ServiciosListaComponent } from './pages/servicios/servicios-lista/servicios-lista/servicios-lista.component';
+import { ServicioDetalleComponent } from './pages/servicios/servicio-detalle/servicio-detalle/servicio-detalle.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'proveedores', pathMatch: 'full' },
   { path: 'proveedores', component: ProveedoresComponent },
   { path: 'clientes', component: ClientesComponent },
   { path: 'empleados', component: EmpleadosComponent },
-  { path: 'servicios', component: ServiciosListaComponent },
+  { 
+    path: 'servicios', 
+    children: [
+      { path: '', component: ServiciosListaComponent },
+      { path: ':id', component: ServicioDetalleComponent }
+    ]
+  },
 ];
